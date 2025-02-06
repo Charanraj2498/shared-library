1636b58f5f76f3cbb7f55fee61b0a6e50eae626c
Charanraj2498 <Charan.r.555@gmail.com>
2025-02-07 02:12:06 +0530

    Update stage_deploy.groovy

--- vars/stage_deploy.groovy
+++ vars/stage_deploy.groovy
@@ -1,11 +1,11 @@
-
-// vars/deployStage.groovy
+// vars/deploy_stage_1.groovy
 
 def call() {
-            sh 'curl ifconfig.me'
-            sh 'curl -L -u "${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW}" -O "http://3.91.150.156:8082/artifactory/jenkins-hello-world-libs-release-local/com/efsavage/hello-world-war/$BUILD_NUMBER/hello-world-war-$BUILD_NUMBER.war"'
-            sh 'ls'
-            sh 'sudo cp hello-world-war-$BUILD_NUMBER.war /opt/tomcat/apache-tomcat-10.1.34/webapps/'
+            sh """
+	    sh 'curl -L -u $ARTIFACTORY_CREDENTIALS_USR:$ARTIFACTORY_CREDENTIALS_PSW -O http://54.196.195.90:8082/artifactory/new-hello-world-war-libs-release/com/efsavage/hello-world-war/${BUILD_NUMBER}/hello-world-war-${BUILD_NUMBER}.war'            
+            sh """
+            sudo cp hello-world-war-${build_number}.war /opt/tomcat/apache-tomcat-10.1.34/webapps/
+            """
             sh 'sudo bash /opt/tomcat/apache-tomcat-10.1.34/bin/shutdown.sh'
             sh 'sudo bash /opt/tomcat/apache-tomcat-10.1.34/bin/startup.sh'
-       }
+}
