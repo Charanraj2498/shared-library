a2578f5dc8feac6f60a4a659ed3cab7f22adb6b3
Charanraj2498 <Charan.r.555@gmail.com>
2025-02-07 02:13:11 +0530

    Update stage_deploy.groovy

--- vars/stage_deploy.groovy
+++ vars/stage_deploy.groovy
@@ -2,7 +2,7 @@
 
 def call() {
             sh """
-	    sh 'curl -L -u $ARTIFACTORY_CREDENTIALS_USR:$ARTIFACTORY_CREDENTIALS_PSW -O http://54.196.195.90:8082/artifactory/new-hello-world-war-libs-release/com/efsavage/hello-world-war/${BUILD_NUMBER}/hello-world-war-${BUILD_NUMBER}.war'            
+	    sh 'curl -L -u $ARTIFACTORY_CREDENTIALS_USR:$ARTIFACTORY_CREDENTIALS_PSW -O http://3.91.150.156:8082/artifactory/jenkins-hello-world-libs-release-local/com/efsavage/hello-world-war/${BUILD_NUMBER}/hello-world-war-${BUILD_NUMBER}.war'            
             sh """
             sudo cp hello-world-war-${build_number}.war /opt/tomcat/apache-tomcat-10.1.34/webapps/
             """
