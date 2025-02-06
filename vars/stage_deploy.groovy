7e5a304b0504bacf63684f868316f0958c125957
Charanraj2498 <Charan.r.555@gmail.com>
2025-02-07 01:52:08 +0530

    Update stage_deploy.groovy

--- vars/stage_deploy.groovy
+++ vars/stage_deploy.groovy
@@ -3,7 +3,7 @@
 
 def call() {
             sh 'curl ifconfig.me'
-            sh 'curl -L -u "${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW}" -O "http://35.175.199.164:8082/artifactory/jenkins-hello-world-libs-release/com/efsavage/hello-world-war/$BUILD_NUMBER/hello-world-war-$BUILD_NUMBER.war"'
+            sh 'curl -L -u "${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW}" -O "http://3.91.150.156:8082/artifactory/jenkins-hello-world-libs-release-local/com/efsavage/hello-world-war/9/$BUILD_NUMBER/hello-world-war-$BUILD_NUMBER.war"'
             sh 'ls'
             sh 'sudo cp hello-world-war-$BUILD_NUMBER.war /opt/tomcat/apache-tomcat-10.1.34/webapps/'
             sh 'sudo bash /opt/tomcat/apache-tomcat-10.1.34/bin/shutdown.sh'
