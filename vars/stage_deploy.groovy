fc9e4e4b2653b165e8909d06c33d7b229c401e75
Charanraj2498 <Charan.r.555@gmail.com>
2025-02-07 03:58:48 +0530

    Update stage_deploy.groovy

--- vars/stage_deploy.groovy
+++ vars/stage_deploy.groovy
@@ -1,12 +1,12 @@
 def call(String build_number, String artifactory_ip) {
             sh 'curl ifconfig.me'
             echo " ${artifactory_ip}"
-            echo "${BUID_NUMBER}"
+            echo "${BUILD_NUMBER}"
             sh """
             curl -L -u "${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW}" -O "http://${artifactory_ip}:8082/artifactory/jenkins-world-libs-release/com/efsavage/hello-world-war/${BUILD_NUMBER}/hello-world-war-${BUILD_NUMBER}.war"
             """
             sh """
-            sudo cp hello-world-war-${build_number}.war /opt/tomcat/apache-tomcat-10.1.34/webapps/
+            sudo cp hello-world-war-${BUILD_NUMBER}.war /opt/tomcat/apache-tomcat-10.1.34/webapps/
             """
             sh 'sudo bash /opt/tomcat/apache-tomcat-10.1.34/bin/shutdown.sh'
             sh 'sudo bash /opt/tomcat/apache-tomcat-10.1.34/bin/startup.sh'
